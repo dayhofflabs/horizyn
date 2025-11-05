6033ac1c55c06e92dbf4e86aef73b945946d7932
Daniel MA <dma@dayhofflabs.com>
2025-11-04 16:32:55 -0800

    Phase 12: Lightning Module

--- horizyn/__init__.py
+++ horizyn/__init__.py
@@ -12,10 +12,23 @@ __version__ = "0.1.0"
 __author__ = "Dayhoff Labs"
 __license__ = "CC BY-NC-SA 4.0"
 
-# Package metadata
+from horizyn.data_module import HorizynDataModule
+from horizyn.lightning_module import HorizynLitModule
+from horizyn.losses import FullBatchMLNCELoss, FullBatchNCELoss
+from horizyn.metrics import create_retrieval_metrics
+from horizyn.model import DualContrastiveModel, MLP
+
+# Package exports
 __all__ = [
     "__version__",
     "__author__",
     "__license__",
+    "HorizynDataModule",
+    "HorizynLitModule",
+    "DualContrastiveModel",
+    "MLP",
+    "FullBatchNCELoss",
+    "FullBatchMLNCELoss",
+    "create_retrieval_metrics",
 ]
 
